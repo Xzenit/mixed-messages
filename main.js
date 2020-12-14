f6b01ce99a2de33801b1863639b6410f78496222
Xzenit <vincent90@gmail.com>
2020-12-14 13:46:38 +0100

    Add basic code and comments

--- main.js
+++ main.js
@@ -1,3 +1,23 @@
 // Mixed Messages - a Portfolio project for Codecademy
 
-console.log('The code is being run succesfully!');
+const beerNames = [];
+const beerTypes = [];
+const breweries = [];
+
+// Function to generate a mixed message with my favourite beer, type and brewery. 
+function generateMixedMessage() {
+    let aOrAn = "";
+    // Pick a random beer from the list
+
+    // Pick a random type from the list
+
+    // Pick a random brewery from the list
+
+    // Check if the beer type starts with a vowel to see if a or an needs to be used
+
+    // Print out the message to the console
+    console.log("My favourite beer is 'Oh my Gose', which is a Gose made by Omnipollo.");
+};
+
+generateMixedMessage();
+
